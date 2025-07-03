c1fb74dbdba3cc9ff541ab176dd21c2ef8e985b0
Evgeniy <userdei@yandex.ru>
2025-07-03 16:41:49 +0300

    fix

--- client_app/urls.py
+++ client_app/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile_view, name='profile'),
-    path('login/', views.login_view, name='login'),
-    path('profile/', views.profile_view, name='profile'),
+    # token
+    #path('login/', views.login_view, name='login'),
+    #path('', views.profile_view, name='profile'),
+    #path('profile/', views.profile_view, name='profile'),
+    # jwt
+    path('login/', views.login_view_jwt, name='login'),
+    path('', views.profile_view_jwt, name='profile'),
+    path('profile/', views.profile_view_jwt, name='profile'),
     path('logout/', views.logout_view, name='logout'),
 ]
--- client_app/views.py
+++ client_app/views.py
@@ -1,9 +1,13 @@
 import requests
 from django.shortcuts import render, redirect
 from django.contrib import messages
+#from client_project.settings import * 
+from django.conf import settings
+
 
 DJOSER_API_URL = "http://127.0.0.1:8088/auth/"
 
+# token
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -30,7 +34,6 @@ def login_view(request):
 
     return render(request, 'login.html')
 
-
 def profile_view(request):
     token = request.session.get('auth_token')
     print(token)
@@ -38,6 +41,50 @@ def profile_view(request):
         return redirect('login')
 
     headers = {'Authorization': f'Token {token}'}
+    response = requests.get(f"{DJOSER_API_URL}users/me/", headers=headers)
+    
+    if response.status_code != 200:
+        messages.error(request, "Ошибка при получении данных пользователя.")
+        return redirect('login')
+
+    user_data = response.json()
+    return render(request, 'profile.html', {'user': user_data})
+
+### jwt
+def login_view_jwt(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Отправляем запрос на Djoser для получения токена
+        response = requests.post(f"{DJOSER_API_URL}jwt/create/", data={
+            'username': username,
+            'password': password
+        })
+        #response = requests.post(f"{DJOSER_API_URL}token/login/", data={
+        #    'username': username,
+        #    'password': password
+        #})        
+
+        if response.status_code == 200:
+            token = response.json()['access']
+            #token = response.json()["auth_token"]
+            request.session['auth_token'] = token
+            messages.success(request, "Вы вошли в систему.")
+            return redirect('profile')
+        else:
+            messages.error(request, "Неверные учетные данные.")
+
+    return render(request, 'login.html')
+
+def profile_view_jwt(request):
+    token = request.session.get('auth_token')
+    if settings.DEBUG:
+        print(token) 
+    if not token:
+        return redirect('login')
+
+    headers = {'Authorization': f'JWT {token}'}
     response = requests.get(f"{DJOSER_API_URL}users/me/", headers=headers)
     
     if response.status_code != 200:
